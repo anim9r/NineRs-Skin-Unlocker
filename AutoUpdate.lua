3c028fe29f76ec581e9918e773782359231bb94c
NineR-9R <76840476+NineR-9R@users.noreply.github.com>
2021-01-07 14:16:32 +0300

    Update AutoUpdate.lua

--- AutoUpdate.lua
+++ AutoUpdate.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NineR-9R/NineRs-Skin-Unlocker/main/Skin-Unlocker-GUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NineR-9R/NineRs-Skin-Unlocker/main/Skin-Unlocker.lua"))()
